d703517e50702775d88cfe461c7773bbdf3b3441
Prakash <prakash.parmar@outlook.com>
2023-11-28 09:57:53 +0100

    feat: adds function documents

--- lib/http_job_processor/jobs.ex
+++ lib/http_job_processor/jobs.ex
@@ -1,27 +1,47 @@
 defmodule HttpJobProcessor.Jobs do
   alias HttpJobProcessor.Jobs.Exceptions.CyclicDepsError
 
-  # tasks = [
-  #   %{
-  #     name: "task-1",
-  #     command: "touch /tmp/file1"
-  #   },
-  #   %{
-  #     name: "task-2",
-  #     command: "cat /tmp/file1",
-  #     requires: ["task-3"]
-  #   },
-  #   %{
-  #     name: "task-3",
-  #     command: "echo 'Hello World!' > /tmp/file1",
-  #     requires: ["task-1"]
-  #   },
-  #   %{
-  #     name: "task-4",
-  #     command: "rm /tmp/file1",
-  #     requires: ["task-2", "task-3"]
-  #   }
-  # ]
+  @doc """
+    A Function to schedule the GIven List of Tasks based on
+  the given dependencies.
+
+  Example:
+
+  iex> tasks = [
+    %{
+      name: "task-1",
+      command: "touch /tmp/file1"
+    },
+    %{
+      name: "task-2",
+      command: "cat /tmp/file1",
+      requires: ["task-3"]
+    },
+    %{
+      name: "task-3",
+      command: "echo 'Hello World!' > /tmp/file1",
+      requires: ["task-1"]
+    },
+    %{
+      name: "task-4",
+      command: "rm /tmp/file1",
+      requires: ["task-2", "task-3"]
+    }
+  ]
+
+  iex> HttpJobProcessor.Jobs.schedule(tasks)
+  {:ok,
+    [
+      %{command: "touch /tmp/file1", name: "task-1"},
+      %{
+        command: "echo 'Hello World!' > /tmp/file1",
+        name: "task-3",
+        requires: ["task-1"]
+      },
+      %{command: "cat /tmp/file1", name: "task-2", requires: ["task-3"]},
+      %{command: "rm /tmp/file1", name: "task-4", requires: ["task-2", "task-3"]}
+    ]}
+  """
   def schedule(tasks) do
     try do
       {:ok, topological_sort(tasks)}
@@ -31,6 +51,49 @@ defmodule HttpJobProcessor.Jobs do
     end
   end
 
+  @doc """
+      An Implementation of the DFS Topological Sorting Algorithm.
+
+      Function also raise exception if given list of tasks has a Cyclic
+    Dependencies.
+
+    Example:
+
+    iex> tasks = [
+      %{
+        name: "task-1",
+        command: "touch /tmp/file1"
+      },
+      %{
+        name: "task-2",
+        command: "cat /tmp/file1",
+        requires: ["task-3"]
+      },
+      %{
+        name: "task-3",
+        command: "echo 'Hello World!' > /tmp/file1",
+        requires: ["task-1"]
+      },
+      %{
+        name: "task-4",
+        command: "rm /tmp/file1",
+        requires: ["task-2", "task-3"]
+      }
+    ]
+
+    iex(5)> HttpJobProcessor.Jobs.topological_sort tasks
+    [
+      %{command: "touch /tmp/file1", name: "task-1"},
+      %{
+        command: "echo 'Hello World!' > /tmp/file1",
+        name: "task-3",
+        requires: ["task-1"]
+      },
+      %{command: "cat /tmp/file1", name: "task-2", requires: ["task-3"]},
+      %{command: "rm /tmp/file1", name: "task-4", requires: ["task-2", "task-3"]}
+    ]
+
+  """
   def topological_sort(tasks) do
     %{ordered: ordered} =
       for task <- tasks, reduce: %{visited: [], ordered: []} do
